8d63418be86be924129834ae41a9f271f83bcea1
dong <dongmingcheng@yuanbaobaoxian.com>
2022-02-09 16:59:27 +0800

    docs(husky):  代码提交格式说明

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -16,3 +16,21 @@ if(!commitRE.test(msg)){
 }else{
     console.log('git commit信息校验通过')
 }
+
+/**
+ * feat: 新功能
+ * fix: 修改bug
+ * dics: 文档
+ * perf: 性能相关
+ * refactor: 代码重构(不影响使用，内部结构调整)
+ * test: 测试用例
+ * style: 样式修改
+ * workflow: 工作流
+ * build: 项目打包构建相关的配置修改
+ * ci: 持续集成相关
+ * revert: 恢复上一次提交(回滚)
+ * wip: work in progress 工作中 还没有完成
+ * chore: 其他修改(不在上述类型中修改)
+ * release: 发版
+ * deps: 依赖相关的修改
+ */
