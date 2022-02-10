35475eb617c481288aedbd7dbd4554cceea3cfcc
dong <dongmingcheng@yuanbaobaoxian.com>
2022-02-10 17:30:16 +0800

    feat(container): 自定义布局组件

---
+++ src/components/container/Aside.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer class="el-aside" :style="{width}">
+        <slot />
+    </footer>
+</template>
+
+<script setup lang="ts" name="ElAside">
+import { withDefaults } from 'vue';
+
+interface Props {
+    width?: string
+}
+
+withDefaults(defineProps<Props>(),{
+    width: '300px'
+})
+
+</script>
+
+<style lang="scss">
+@import '../styles/mixin.scss';
+@include b(aside) {
+    overflow: auto;
+    box-sizing: border-box;
+    flex-shrink: 0;
+}
+
+</style>
---
+++ src/components/container/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <footer class="el-footer" :style="{height}">
+        <slot />
+    </footer>
+</template>
+
+<script setup lang="ts" name="ElFooter">
+import { withDefaults } from 'vue';
+
+interface Props {
+    height?: string
+}
+
+withDefaults(defineProps<Props>(),{
+    height: '60px'
+})
+
+</script>
+
+<style lang="scss">
+@import '../styles/mixin.scss';
+@include b(footer) {
+    padding: $--footer-padding;
+    box-sizing: border-box;
+    flex-shrink: 0;
+    @include m(inner) {
+        width: 300px
+    }
+}
+
+</style>
